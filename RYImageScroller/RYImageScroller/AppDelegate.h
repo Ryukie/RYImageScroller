17df8237cf12d77aba715907bac06669d07600a6
Ryukie <ryukiewang@qq.com>
2016-09-04 15:12:11 +0800

    init

---
+++ RYImageScroller/RYImageScroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RYImageScroller
+//
+//  Created by RongqingWang on 16/9/4.
+//  Copyright © 2016年 RongqingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RYImageScroller/RYImageScroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RYImageScroller
+//
+//  Created by RongqingWang on 16/9/4.
+//  Copyright © 2016年 RongqingWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
